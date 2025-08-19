0f786ff980bba1cc230a0f054c19f6a541145122
Dhanushri26 <ds20262005@gmail.com>
2025-08-19 21:15:51 +0530

    divided page

--- src/Page.jsx
+++ src/Page.jsx
@@ -12,10 +12,7 @@ import {
 import { BarChart } from "@mui/x-charts/BarChart";
 const styles = {
   Page: {
-    padding: "2rem",
-    backgroundColor: "#f9f9f9",
-    minHeight: "100vh",
-    fontFamily: "'Nunito', sans-serif",
+    fontFamily: "poppins",
   },
   Header: { textAlign: "center", marginBottom: "2rem" },
   HeaderText: { fontSize: "2rem", fontWeight: "bold" },
@@ -64,82 +61,86 @@ const Page = () => {
     setShowGradePoint(true);
   };
   return (
-    <Box sx={styles.Page}>
-      <Box sx={styles.Header}>
-        <Typography sx={styles.HeaderText}>Grade Point Calculator</Typography>
-      </Box>
+    <Grid container justifyContent="center" sx={{ fontFamily: "poppins" }}>
+      <Box sx={styles.Page}>
+        <Box sx={styles.Header}>
+          <Typography sx={styles.HeaderText}>Grade Point Calculator</Typography>
+        </Box>
 
-      {marks.map((mark, index) => (
-        <Grid
-          container
-          key={index}
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          sx={{ marginTop: "20px" }}
-        >
-          <Grid item>
-            <TextField
-              label="Enter Subject"
-              variant="outlined"
-              required
-              value={mark.subject}
-              onChange={(e) => handleChange(index, "subject", e.target.value)}
-              sx={{ backgroundColor: "#fff", width: "300px" }}
-            />
-          </Grid>
-          <Grid item>
-            <FormControl
-              variant="outlined"
-              sx={{ backgroundColor: "#fff", width: "300px" }}
-            >
-              <Select
-                value={mark.grade}
-                onChange={(e) => handleChange(index, "grade", e.target.value)}
-                displayEmpty
+        {marks.map((mark, index) => (
+          <Grid
+            container
+            key={index}
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            sx={{ marginTop: "20px" }}
+          >
+            <Grid item>
+              <TextField
+                label="Enter Subject"
+                variant="outlined"
+                required
+                value={mark.subject}
+                onChange={(e) => handleChange(index, "subject", e.target.value)}
+                sx={{ backgroundColor: "#fff", width: "300px" }}
+              />
+            </Grid>
+            <Grid item>
+              <FormControl
+                variant="outlined"
+                sx={{ backgroundColor: "#fff", width: "300px" }}
               >
-                <MenuItem value="" disabled>
-                  Select Grade
-                </MenuItem>
-                {grades.map((grade) => (
-                  <MenuItem key={grade.grade} value={grade.value}>
-                    {grade.grade}
+                <Select
+                  value={mark.grade}
+                  onChange={(e) => handleChange(index, "grade", e.target.value)}
+                  displayEmpty
+                >
+                  <MenuItem value="" disabled>
+                    Select Grade
                   </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+                  {grades.map((grade) => (
+                    <MenuItem key={grade.grade} value={grade.value}>
+                      {grade.grade}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
           </Grid>
-        </Grid>
-      ))}
+        ))}
 
-      <Grid container justifyContent="center" sx={{ marginTop: "30px" }}>
-        <Button variant="contained" onClick={handleAddSubject}>
-          + Add Subject
-        </Button>
-        <Button
-          variant="contained"
-          onClick={handleCalculateGPA}
-          sx={{ marginLeft: "20px" }}
-        >
-          Calculate GPA
-        </Button>
-      </Grid>
-      <Grid container justifyContent="center" sx={{ marginTop: "30px" }}>
-        {showGradePoint && (
-          <Box>
-            <Typography variant="h6">
-              Total GPA: {totalMarks ? totalMarks : "0.00"}
-            </Typography>
-          </Box>
-        )}
-      </Grid>
-      <BarChart
-        xAxis={[{ data: marks.map((mark) => mark.subject) }]}
-        series={[{ data: marks.map((mark) => mark.grade) }]}
-        width={400}
-        height={300}
-      />
-    </Box>
+        <Grid container justifyContent="center" sx={{ marginTop: "30px" }}>
+          <Button variant="contained" onClick={handleAddSubject}>
+            + Add Subject
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleCalculateGPA}
+            sx={{ marginLeft: "20px" }}
+          >
+            Calculate GPA
+          </Button>
+        </Grid>
+        <Grid container justifyContent="center" sx={{ marginTop: "30px" }}>
+          {showGradePoint && (
+            <Box>
+              <Typography variant="h6">
+                Total GPA: {totalMarks ? totalMarks : "0.00"}
+              </Typography>
+            </Box>
+          )}
+        </Grid>
+      </Box>
+      <Box>
+        <BarChart
+          xAxis={[{ data: marks.map((mark) => mark.subject) }]}
+          series={[{ data: marks.map((mark) => mark.grade) }]}
+          width={400}
+          height={300}
+        />
+      </Box>
+    </Grid>
   );
 };
 
